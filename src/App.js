4b7def4b0b1332f39e4f8ef0767c0e20d5ddbf80
Sushil Choudhari <sushilchoudhari9922@gmail.com>
2021-09-29 17:54:08 +0530

    feat  : added onclick funtion added  onclick function and also linked it with a database

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./styles.css";
 
 var places = {
@@ -41,10 +42,37 @@ var places = {
   ]
 };
 
+var placeslist = Object.keys(places);
+
 export default function App() {
+  var [country, setCountry] = useState("India");
+
+  function onClickhandler(item) {
+    setCountry(item);
+  }
+
   return (
     <div className="App">
       <h1>Travel Recommendation </h1>
+      <div>text</div>
+      <div>
+        {placeslist.map((item) => (
+          <button key={item} onClick={() => onClickhandler(item)}>
+            {item}
+          </button>
+        ))}
+      </div>
+      <hr />
+      <div>
+        <ul style={{ listStyle: "none" }}>
+          {places[country].map((place) => (
+            <li key={place.name}>
+              <div>{place.name}</div>
+              <div>{place.location}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
