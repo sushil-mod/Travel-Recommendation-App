3feda8dc5cff8994b62d3b52c9dafb6d95ace806
Sushil Choudhari <sushilchoudhari9922@gmail.com>
2021-09-29 17:08:52 +0530

    added database

--- src/App.js
+++ src/App.js
@@ -1,5 +1,50 @@
 import "./styles.css";
 
+var places = {
+  India: [
+    {
+      name: "Taj mahal",
+      location: "Agra,Uttar Pradesh"
+    },
+    {
+      name: "The golden Temple",
+      location: "Amritsar, Punjab"
+    },
+    {
+      name: "The Ellora Caves",
+      location: "Ellora, Maharashtra"
+    }
+  ],
+  Spain: [
+    {
+      name: "Guggenheim Musseum",
+      location: " Bilbao"
+    },
+    {
+      name: "The Alhambra and Generalife Gardens",
+      location: " Granada"
+    }
+  ],
+  France: [
+    {
+      name: "Eiffel Tower",
+      location: "Paris"
+    },
+    {
+      name: " Louvre Museum",
+      location: "Paris"
+    },
+    {
+      name: "Palace of Versailles",
+      location: "Versailles"
+    }
+  ]
+};
+
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Travel Recommendation </h1>
+    </div>
+  );
 }
